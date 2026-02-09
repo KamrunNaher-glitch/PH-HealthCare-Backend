2bb5ab841e89e53cf9a2985d80d03c601aa42cda
KamrunNaher-glitch <keyachandani7@gmail.com>
2026-02-09 23:07:04 +0600

    Handle Native Errors of JS

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import status from "http-status";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import z from "zod";
+import { handleZodError } from "../errorHelpers/handleZodError";
+import AppError from "../errorHelpers/appError";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunction) =>{
@@ -8,12 +12,50 @@ export const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunct
         console.log("Error From Global Error Handler",err)
     }
    
-    const statusCode:number = status.INTERNAL_SERVER_ERROR
-    const message :string = "Internal Server Error"
+      let errorSources: TErrorSources[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal Server Error';
+    let stack: string | undefined = undefined;
 
-    res.status(statusCode).json({
-        success:false,
-        message:message,
-        error:err.message
-    })
+
+     if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
+        stack = err.stack;
+
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+    else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+
+
+    const errorResponse: TErrorResponse = {
+        success: false,
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === 'development' ? err : undefined,
+        stack: envVars.NODE_ENV === 'development' ? stack : undefined,
+    }
+
+    res.status(statusCode).json(errorResponse);
 }
