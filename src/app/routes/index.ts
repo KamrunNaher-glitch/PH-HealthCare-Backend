8ca783ac1ac2559fcc88d56b3c83b35a438d72d2
KamrunNaher-glitch <keyachandani7@gmail.com>
2026-03-02 11:34:30 +0600

    Implement Schedule service and validation

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,11 +3,17 @@ import { SpecialtyRoutes } from "../modules/speciality/speciality.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { UserRoutes} from "../modules/user/user.route";
 import { DoctorRoutes } from "../modules/doctor/doctor.route";
+import { AdminRoutes } from "../modules/admin/admin.route";
+import { scheduleRoutes } from "../modules/schedule/schedule.route";
 
 const router = Router();
 router.use("/auth",AuthRoutes)
 router.use("/specialties",SpecialtyRoutes)
 router.use("/users",UserRoutes);
-router.use("/doctors",DoctorRoutes)
+router.use("/doctors",DoctorRoutes);
+router.use("/admins", AdminRoutes)
+router.use("/schedules", scheduleRoutes)
+router.use("/doctor-schedules", DoctorScheduleRoutes)
+router.use("/appointments", AppointmentRoutes)
 
 export const IndexRoutes= router;
